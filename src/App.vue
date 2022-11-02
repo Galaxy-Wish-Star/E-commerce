971b7336f5e9098cdb5c42c0dcb43b3cccefd2ff
mason369 <maosenyang00@gmail.com>
2022-11-02 16:56:49 +0800

    update public
    更新主流js插件与移动端px转换vw插件

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-
+    <div class="test-viewport">测试转换</div>
   </div>
 </template>
 
@@ -11,5 +11,12 @@
 </script>
 
 <style lang="scss" scoped>
-
+.test-viewport {
+  width: 750px;
+  height: 100px;
+  font-size: 40px;
+  text-align: center;
+  line-height: 100px;
+  background: #13b5b1;
+}
 </style>
