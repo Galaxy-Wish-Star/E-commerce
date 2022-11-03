4a3fdd9816b465ef3242e9c2c774d0102628ce57
mason369 <maosenyang00@gmail.com>
2022-11-03 14:58:11 +0800

    update test

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="root">
-    
+    <div class="test-viewport">测试转换</div>
   </div>
 </template>
 
 <script>
+// import axios from "axios"
   export default {
     name:"App",
     comments:{
-      
+
     }
   }
 </script>
 
 <style lang="scss" scoped>
-
+.test-viewport {
+  width: 750px;
+  height: 100px;
+  font-size: 40px;
+  text-align: center;
+  line-height: 100px;
+  background: #13b5b1;
+}
 </style>
