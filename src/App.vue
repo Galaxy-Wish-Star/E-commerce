d24a10f3305a5db32c3a14f021c931d9a843ea2b
mason369 <maosenyang00@gmail.com>
2022-11-03 10:04:18 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,18 @@
 <template>
-  <div>
-    <div class="test-viewport">测试转换</div>
+  <div class="root">
+    
   </div>
 </template>
 
 <script>
   export default {
-    
+    name:"App",
+    comments:{
+      
+    }
   }
 </script>
 
 <style lang="scss" scoped>
-.test-viewport {
-  width: 750px;
-  height: 100px;
-  font-size: 40px;
-  text-align: center;
-  line-height: 100px;
-  background: #13b5b1;
-}
+
 </style>
