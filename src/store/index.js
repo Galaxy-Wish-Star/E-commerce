6a578d4cd6fc2aed33bb76cd452d11eb4b565127
mason369 <maosenyang00@gmail.com>
2022-11-04 09:15:54 +0800

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@
  */
 
 //引入Vuex
-import Vuex from 'vuex';
+import Vuex from "vuex";
 import Vue from "vue";
 
 //使用vuex来集中管理状态,必要
@@ -12,65 +12,18 @@ Vue.use(Vuex);
 
 //创建actions(本质就是对象) 用于响应组件中的动作
 const actions = {
-    //收到上下文对象(包含commit)和dispatch过来的值
-    // increment(context, value){
-    //     context.commit('INCREMENT', value);
-    // },
-    // decrement(context, value){
-    //     context.commit('DECREMENT', value);
-    // },
-    incrementIfOdd(context, value){
-        // console.log(`action中的incrementIfOdd被调用`);
-        // console.log('处理了一些事情');
-        // context.dispatch('demo1', value);
-        if(context.state.sum % 2) {
-            console.log('@')
-            context.commit('INCREMENT',value);
-            // context.state.sum += 1;//这样可以实现但是记住本次对状态的改变开发者工具将无法捕获，因为开发者工具是对mutations对话的
-        }
-    },
-    incrementWait(context, value){
-        setTimeout(() => {
-            context.commit('INCREMENT', value);
-        },500)
-    },
-  
-}
+	//收到上下文对象(包含commit)和dispatch过来的值
+};
 
 //创建mutations(本质也是对象) 用于修改数据(state)
-const mutations = {
-    //收到state和要操作数value
-    INCREMENT(state, value) {
-        state.sum += value;
-    },
-    DECREMENT(state, value) {
-        state.sum -= value;
-    },
-}
+const mutations = {};
 
 //准备getters用于加工state，将其共享于各个组件当中
-const getters = {
-    bigSum(state){
-        return state.sum * 10;
-    }
-}
+const getters = {};
 
 //准备state(数据) 存储数据
 //类似于各个组件里的computed(计算属性),只不过它是共享的
-const state = {
-    sum: 0,
-    school: 'Wust',
-    subject: 'Computer Science',
-}
-
+const state = {};
 
 //创建并暴露store
-export default new Vuex.Store({
-    actions,
-    mutations,
-    state,
-    getters
-});
-
-
-
+export default new Vuex.Store({});
