8cd5b76e3427d544ad1a22188a5b4157b8166531
mason369 <maosenyang00@gmail.com>
2022-11-04 16:18:24 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 // import store from './store'
 
+//引入vue router
+import VueRouter from 'vue-router'
 
+//使用插件
+Vue.use(VueRouter)
 new Vue({
   render: h => h(App),
   // store,
