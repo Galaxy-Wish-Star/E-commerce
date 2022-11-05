22a83388382121027b08bc07d71dec7308d6d6a6
mason369 <maosenyang00@gmail.com>
2022-11-05 21:08:07 +0800

    update

--- src/main.js
+++ src/main.js
@@ -3,10 +3,16 @@ import App from './App.vue'
 // import store from './store'
 
 //引入vue router
-import VueRouter from 'vue-router'
+// import VueRouter from 'vue-router'
 
-//使用插件
-Vue.use(VueRouter)
+//使用路由插件
+// Vue.use(VueRouter)
+
+//使用element ui插件
+// import ElementUI from 'element-ui';
+// import 'element-ui/lib/theme-chalk/index.css';
+// Vue.use(ElementUI);
+
 new Vue({
   render: h => h(App),
   // store,
