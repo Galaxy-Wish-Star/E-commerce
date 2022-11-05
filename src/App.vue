68ce047e6b11a951e3b38d9f254ef8c46560e0bc
mason369 <maosenyang00@gmail.com>
2022-11-05 23:27:09 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="root">
-    <Header></Header>
-    <div class="test-viewport">测试转换</div>
-  </div>
+    <div class="root">
+        <Header></Header>
+        <Footer></Footer>
+        <div class="test-viewport">测试转换</div>
+    </div>
 </template>
 
 <script>
@@ -10,23 +11,26 @@
 // import bootstrap from "bootstrap"
 
 // 引入页面
-import Header from "./components/Header/Header"
-  export default {
-    name:"App",
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
+export default {
+    name: "App",
     //注册组件
-    comments:{
-  Header,
-    }
-  }
+    comments: {
+        Header,
+        Footer,
+    },
+};
 </script>
 
 <style lang="scss" scoped>
 .test-viewport {
-  width: 750px;
-  height: 100px;
-  font-size: 40px;
-  text-align: center;
-  line-height: 100px;
-  background: #13b5b1;
+    width: 750px;
+    height: 100px;
+    font-size: 40px;
+    text-align: center;
+    line-height: 100px;
+    background: #13b5b1;
 }
 </style>
