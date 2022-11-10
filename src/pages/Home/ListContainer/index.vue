02ef92839479e51c3e18076a75d4aca600eb8286
mason369 <maosenyang00@gmail.com>
2022-11-10 10:22:39 +0800

    update

--- src/pages/Home/ListContainer/index.vue
+++ src/pages/Home/ListContainer/index.vue
@@ -6,7 +6,7 @@
                 <div class="swiper-container" id="mySwiper">
                     <div class="swiper-wrapper">
                         <div class="swiper-slide">
-                            <img src="./images/banner1.jpg" />
+                            <img src="./images/banner2.jpg" />
                         </div>
                         <!-- <div class="swiper-slide">
                             <img src="./images/banner2.jpg" />
