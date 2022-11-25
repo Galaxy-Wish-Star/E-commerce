b1a06614376b845d0d2349a7eb44c0f336fb7e33
mason369 <maosenyang00@gmail.com>
2022-11-25 14:48:13 +0800

    update

--- src/mock/mockServe.js
+++ src/mock/mockServe.js
@@ -6,7 +6,9 @@ import Mock from "mockjs";
 import banner from "./banner.json";
 import floor from "./floor.json";
 import floorBanner from  './floorBanner.json'
+import goodsList from "./goodsList.json"
 
 Mock.mock("/mock/banner", { code: 200, data: banner });//模拟首页大的轮播图的数据
 Mock.mock("/mock/floor", { code: 200, data: floor});
 Mock.mock("/mock/floorBanner" ,{code: 200, data: floorBanner});//模拟floor模块轮播图数据
+Mock.mock("/mock/goodsList" ,{code: 200, data: goodsList});//模拟热门商品数据
--- vue.config.js
+++ vue.config.js
@@ -20,12 +20,6 @@ module.exports = {
                 ws: true, //支持websocket
                 changeOrigin: true, //host源
             },
-            "/ipa": {
-                target: "http://10.31.1.65:8080", //代理转发路径
-                pathRewrites: { "^/ipa": "" }, //匹配字符串
-                ws: true, //支持websocket
-                changeOrigin: true, //host源
-            },
         },
     },
 };
