b78d9bea80903d2fcb1179f7b5d871df32d759be
mason369 <maosenyang00@gmail.com>
2022-12-02 23:20:35 +0800

    Update index.js

--- src/admin/build/index.js
+++ src/admin/build/index.js
@@ -4,6 +4,13 @@ const config = require('../vue.config.js')
 const rawArgv = process.argv.slice(2)
 const args = rawArgv.join(' ')
 
+const { run } = require('runjs')
+const chalk = require('chalk')
+const config = require('../vue.config.js')
+const rawArgv = process.argv.slice(2)
+const args = rawArgv.join(' ')
+
+
 if (process.env.npm_config_preview || rawArgv.includes('--preview')) {
   const report = rawArgv.includes('--report')
 
